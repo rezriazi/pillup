d744c46ee73c3b9a5a93269b4a651477deb08b36
amir <amirdrj7@students.cs.ubc.ca>
2020-02-15 02:47:44 -0800

    amookhosro_platform

--- src/gamejam/model/Platform.java
+++ src/gamejam/model/Platform.java
@@ -1,14 +1,27 @@
 package gamejam.model;
 
-public class Platform extends Object {
+public class Platform extends Object implements Updatable, Drawable {
 
-    @Override
-    public <T> void move(T... obj) {
-
+    Platform(double x, double y, double w, double h, double yVel) {
+        super(x, y, w, h, 0, yVel);
     }
 
     @Override
-    public <T> void jump(T... obj) {
+    public <T> void move(T... obj) {
+        this.setY(this.getY() + this.getyVel());
+    }
 
+    @Override
+    public <T> void jump(T... obj) {}
+
+    // TODO
+    @Override
+    public <T> void draw(T... obj) {
+        // get the gc and draw the image at its position
+    }
+
+    @Override
+    public <T> void update(T... obj) {
+        move(obj);
     }
 }
