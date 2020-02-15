65ed3b153a98bcc1079e6a4fb62e9bcfccd4095e
amir <amirdrj7@students.cs.ubc.ca>
2020-02-15 03:28:50 -0800

    amookhosro_kosro

--- src/gamejam/ui/Game.java
+++ src/gamejam/ui/Game.java
@@ -1,10 +1,58 @@
 package gamejam.ui;
 
 import gamejam.model.Drawer;
+import gamejam.model.PlatformManager;
+import gamejam.model.Player;
+import gamejam.model.Updatable;
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.GraphicsContext;
 
-public class Game implements Drawer{
+public class Game implements Drawer, Updatable {
+
+    private Player player;
+    private PlatformManager platformManager;
+    private ObstacleManager obstacleManager;
+    private GraphicsContext gc;
+
+    private boolean running = false;
+    private long dt = 10;
+
+    public Game(GraphicsContext gc) {
+        this.player = new Player();
+        this.platformManager = new PlatformManager();
+        this.obstacleManager = new ObstacleManager();
+        this.gc = gc;
+    }
+
     @Override
     public void draw() {
+        player.draw(gc);
+        platformManager.draw(gc);
+        obstacleManager.draw(gc);
+    }
 
+    @Override
+    public <T> void update(T... obj) {
+        player.update();
+        platformManager.update();
+        obstcaleManager.update();
+    }
+
+    public void start() {
+        running = true;
+        new AnimationTimer() {
+            private long lastUpdate = 0;
+
+            @Override
+            public void handle(long now) {
+                if (running) {
+                    if ((now - lastUpdate) >= dt) {
+                        // TODO: getting input
+                        update();
+                        draw();
+                    }
+                }
+            }
+        }.start();
     }
 }
