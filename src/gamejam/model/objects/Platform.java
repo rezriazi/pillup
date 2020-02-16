1712c2c4e08f42bd59b3ff9b1dd7a3a04c129357
sepehr541 <sepehr541@gmail.com>
2020-02-16 02:04:51 -0800

    pills_added

--- src/gamejam/model/objects/Platform.java
+++ src/gamejam/model/objects/Platform.java
@@ -2,20 +2,33 @@ package gamejam.model.objects;
 
 import gamejam.model.interfaces.Drawable;
 import gamejam.model.interfaces.Updatable;
+import gamejam.model.managers.PlatformManager;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
+import java.io.FileInputStream;
 import java.util.Random;
 
 public class Platform extends Object implements Updatable, Drawable {
 
+
+
+    private static final int PILL_WIDTH = 50;
+    private static final int PILL_HEIGHT = 20;
+
     enum types {
-        FOOD,
         PILL,
-        DEGREE,
-        MONEY,
+        SPIKE,
     }
 
-    private types type = types.FOOD;
+    enum PillColors {
+        BLUE,
+        RED,
+        YELLOW,
+    }
+
+    private types type = types.PILL;
+    private PillColors pillColor = PillColors.BLUE;
     public Platform(double x, double y, double w, double h, double yVel) {
         super(x, y, w, h, 0, yVel);
         changeType();
@@ -34,7 +47,22 @@ public class Platform extends Object implements Updatable, Drawable {
     public <T> void draw(T... obj) {
         // get the gc and draw the image at its position
         GraphicsContext gc = (GraphicsContext) obj[0];
-        gc.fillRect(getX(),getY(),getW(),getH());
+        if (this.type == types.SPIKE) {
+            gc.fillRect(getX(),getY(),getW(),getH());
+        } else {
+            switch (this.pillColor){
+                case RED:
+                    gc.drawImage(PlatformManager.redImg, this.getX(), this.getY(), PILL_WIDTH, PILL_HEIGHT);
+                    break;
+                case BLUE:
+                    gc.drawImage(PlatformManager.blueImg, this.getX(), this.getY(), PILL_WIDTH, PILL_HEIGHT);
+                    break;
+                case YELLOW:
+                    gc.drawImage(PlatformManager.yellowImg, this.getX(), this.getY(), PILL_WIDTH, PILL_HEIGHT);
+                    break;
+            }
+        }
+
     }
 
     @Override
@@ -45,6 +73,11 @@ public class Platform extends Object implements Updatable, Drawable {
     public void changeType() {
         Random random = new Random();
         this.type = types.values()[random.nextInt(types.values().length)];
+        if (this.type == types.PILL) {
+            this.pillColor = PillColors.values()[random.nextInt(PillColors.values().length)];
+        } else {
+            this.pillColor = PillColors.BLUE;
+        }
     }
 
 }
