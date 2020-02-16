caf3d0038ca7c0503098e83c62601e7303a81549
Moh <moh10moh78@gmail.com>
2020-02-16 07:16:30 -0800

    navid_major_player_change_10

--- src/gamejam/model/objects/Player.java
+++ src/gamejam/model/objects/Player.java
@@ -15,6 +15,22 @@ public class Player extends Object {
             System.getProperty("user.dir") +
                     "/src/gamejam/assets/littleboy2!.png";
 
+    // adult age
+    private static final String ADULT_PATH =
+            System.getProperty("user.dir") +
+                    "/src/gamejam/assets/adultage1!.png";
+    private static final String ADULT_PATH_1 =
+            System.getProperty("user.dir") +
+                    "/src/gamejam/assets/adultage2!.png";
+
+
+    private static final String OLD_PATH =
+            System.getProperty("user.dir") +
+                    "/src/gamejam/assets/oldage1!.png";
+    private static final String OLD_PATH_1 =
+            System.getProperty("user.dir") +
+                    "/src/gamejam/assets/oldage2!.png";
+
     private static final long JUMP_ANIMATION = 250;
     private static final long X_ANIMATION = 150;
     private static final double PLAYER_HEIGHT = 60;
@@ -23,20 +39,60 @@ public class Player extends Object {
     private static final double PLAYER_INIT_Y = Main.HEIGHT - 2 * PLAYER_HEIGHT;
 
 
+    private static final double RED_PILL_RATIO = 20;
+
+    enum Age {
+        YOUNG,
+        ADULT,
+        OLD
+    }
+
     private Animation action;
     private Animation actionX;
 
-
+    private Age age;
+    int ageCount;
 
     public Player(){
         super(PLAYER_INIT_X,PLAYER_INIT_Y, PLAYER_HEIGHT, PLAYER_WIDTH, 5,-5);
-        action = new Animation(JUMP_ANIMATION);
-        actionX = new Animation(X_ANIMATION,LITTLE_BOY_PATH,LITTLE_BOY_PATH_1);
+        changeCharacter(Age.YOUNG);
+    }
+
+    public Player(Age a){
+        super(PLAYER_INIT_X,PLAYER_INIT_Y, PLAYER_HEIGHT, PLAYER_WIDTH, 5,-5);
+        changeCharacter(a);
+    }
+
+
+    public void changeCharacter(Age a) {
+        this.age = a;
+        String[] paths = getPathsForAge();
+        action = new Animation(JUMP_ANIMATION,paths);
+        actionX = new Animation(X_ANIMATION,paths);
         try {
-            this.setImage(LITTLE_BOY_PATH);
+            this.setImage(paths[0]);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private String[] getPathsForAge() {
+        String[] s = new String[2];
+        switch (this.age){
+            case YOUNG:
+                s[0] =  LITTLE_BOY_PATH;
+                s[1] = LITTLE_BOY_PATH_1;
+                break;
+            case ADULT:
+                s[0] = ADULT_PATH;
+                s[1] = ADULT_PATH_1;
+                break;
+            case OLD:
+                s[0] = OLD_PATH;
+                s[1] = OLD_PATH_1;
+                break;
+        }
+        return s;
     }
 
 
@@ -44,7 +100,12 @@ public class Player extends Object {
     public <T> void draw(T... obj) {
         GraphicsContext gc = (GraphicsContext) obj[0];
        // gc.drawImage(this.getImage(),this.getX(),this.getY(),this.getW(),this.getH());
-        gc.drawImage(this.actionX.getCurrentImage(),this.getX(),this.getY(),this.getW(),this.getH());
+        if(actionX.isInAction()){
+            gc.drawImage(this.actionX.getCurrentImage(),this.getX(),this.getY(),this.getW(),this.getH());
+        }else{
+            gc.drawImage(this.action.getCurrentImage(),this.getX(),this.getY(),this.getW(),this.getH());
+        }
+
     }
 
     @Override
@@ -77,8 +138,42 @@ public class Player extends Object {
             this.moveX();
         }
 
+        this.updateCharacter();
         this.inBound();
 
+    }
+
+
+    private void updateCharacter() {
+        if(ageToInt()!=this.ageCount){
+            setAgeFromInt(this.ageCount);
+        }
+    }
+
+    private int ageToInt(){
+        switch (age){
+            case ADULT:
+                return 1;
+            case YOUNG:
+                return 0;
+            case OLD:
+                return 2;
+        }
+        return 0;
+    }
+
+    private void setAgeFromInt(int age){
+        switch (age){
+            case 0:
+                this.changeCharacter(Age.YOUNG);
+                break;
+            case 1:
+                this.changeCharacter(Age.ADULT);
+                break;
+            case 2:
+                this.changeCharacter(Age.OLD);
+                break;
+        }
     }
 
     private void inBound() {
@@ -96,17 +191,53 @@ public class Player extends Object {
     }
 
 
-    public void pill(String type){
+    // retruns whether they should loose or not after taking this pill
+    public boolean pill(String type){
         switch (type){
             case "red":
                 //bigger
-                break;
+                return redPill();
             case "blue":
                 //older
-                break;
+                return bluePill();
             case "yellow":
                 //younger
-                break;
+                return yellowPill();
         }
+        return false;
     }
+
+    private boolean redPill(){
+        this.setW(this.getW() + RED_PILL_RATIO);
+        this.setH(this.getH() + RED_PILL_RATIO);
+        if (this.getY() + this.getH() >= Main.GROUND) {
+            this.setY(Main.GROUND - this.getH());
+        }
+        return false;
+    }
+
+    private boolean bluePill() {
+        //older
+        this.ageCount++;
+        if(this.ageCount>2) {
+            // they should loose
+            ageCount = 2;
+            return true;
+        }
+        return false;
+    }
+
+
+    private boolean yellowPill(){
+        //younger
+        this.ageCount --;
+        if(this.ageCount < 0){
+            // should loose
+            ageCount = 0;
+            return true;
+        }
+        return false;
+    }
+
+
 }
